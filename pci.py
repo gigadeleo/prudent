8af5283f7407665f8b404fc4568880b3e9518574
Giannella De Leonardo <gdeleonardo@gmail.com>
2017-01-16 23:38:03 +0100

    stability improvements

--- pci.py
+++ pci.py
@@ -8,10 +8,8 @@ conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 def lookup (searchstring):
-    print searchstring
     c.execute ('SELECT tblPCIDSS32.req_detail from tblPCIDSS32 WHERE tblPCIDSS32.req_detail LIKE \'%' + searchstring + '%\' ')
     results = c.fetchall()
-    for r in results:
-        print r
+
     return results
     #return results
